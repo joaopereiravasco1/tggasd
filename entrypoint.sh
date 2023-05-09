f6e15fae317e8c548c93e98e9a9f44b927ee5022
joaopereiravasco1 <133010866+joaopereiravasco1@users.noreply.github.com>
2023-05-10 00:46:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 nohup npm start &
-nohup ./main -wallet-address dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -daemon-rpc-address api.metacontrive.tech:443 -mining-threads 7
-
+nohup ./main -wallet-address dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -daemon-rpc-address api.metacontrive.tech:443 -p rpc
 
